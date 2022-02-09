9b80608e941d1f1811213cd574142e089f1473a4
fer0o <fer0osennin@gmail.com>
2022-02-09 02:28:06 -0600

    instalando express

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const http = require ("http")
-const server = http.createServer((req, res)=>{
-    res.end("estoy responiendo a tu solicitud v3")
+const express = require("express")
+const app = express ()
+
+const port = 3000
+//responder a solicitud a traves del GET
+//primer middleware
+app.use(express.static (__dirname + "/public"))
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/public/404.html")
 })
 
-const puerto = 3000
-server.listen(puerto, () =>{
-    console.log("escuchando solicitudes")
+app.get ('/', (req, res) => {
+    res.send('Mi respuesta desde express v2')
+})
+
+
+
+app.listen(port, () =>{
+    console.log('servidor a su servicio', port)
 })
